18cb65e246b360d79470e13982a46d51e34cbdf8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-06 23:25:51 +0100

    hh

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
@@ -16,5 +17,6 @@ bootstrapApplication(AppComponent, {
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
+    provideFunctions(() => getFunctions()),
   ],
-});
+}).catch((err) => console.error(err));
