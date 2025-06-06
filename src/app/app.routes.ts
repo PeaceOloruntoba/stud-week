ca81df4a96a6aee15a156b02c2c71544d8129503
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-06 17:47:57 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,28 +4,22 @@ import { AuthGuard } from './modules/auth/guards/auth.guard';
 // import { ProfileComponent } from './modules/user/components/profile/profile.component'; // Remove direct imports
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   {
     path: 'auth',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
-    path: 'users',
+    path: '',
     loadChildren: () =>
-      import('./modules/users/users.module').then((m) => m.UsersModule),
-  },
-  {
-    path: 'company',
-    loadChildren: () =>
-      import('./modules/company/company.module').then((m) => m.CompanyModule),
+      import('./modules/user/user.module').then((m) => m.UserModule),
   },
   {
     path: 'admin',
     loadChildren: () =>
       import('./modules/admin/admin.module').then((m) => m.AdminModule),
   },
-  { path: '**', redirectTo: '/auth/login' },
+  { path: '**', redirectTo: '/login' },
   {
     path: 'unauthorized',
     loadComponent: () =>
