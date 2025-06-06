708c5f5b10af38477451bb65abaa6b91ecf8b75a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-06 18:00:51 +0100

    Update auth.module.ts

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    LoginComponent,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      { path: 'login', component: LoginComponent },
+    ]),
+  ],
+  declarations: [],
 })
-export class AuthModule { }
+export class AuthModule {}
