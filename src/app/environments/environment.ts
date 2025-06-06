3a49f6fbe045007a378bcdee718232bd5e959119
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-06 17:16:18 +0100

    gg

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,9 +1,12 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyDD9eOfrCt-OLZWnW8yAAnerF3RdS0lqvw',
-  authDomain: 'stud-event.firebaseapp.com',
-  projectId: 'stud-event',
-  storageBucket: 'stud-event.firebasestorage.app',
-  messagingSenderId: '141072434099',
-  appId: '1:141072434099:web:07c61042e475cd3ed501c6',
-  measurementId: 'G-TPXH3GVDH8',
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDD9eOfrCt-OLZWnW8yAAnerF3RdS0lqvw',
+    authDomain: 'stud-event.firebaseapp.com',
+    projectId: 'stud-event',
+    storageBucket: 'stud-event.firebasestorage.app',
+    messagingSenderId: '141072434099',
+    appId: '1:141072434099:web:07c61042e475cd3ed501c6',
+    measurementId: 'G-TPXH3GVDH8',
+  },
 };
