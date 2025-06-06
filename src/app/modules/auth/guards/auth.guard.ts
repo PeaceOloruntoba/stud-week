813d62b359c0e425014d94fc1ba1db522458066d
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-06 21:53:32 +0100

    Update auth.guard.ts

--- src/app/modules/auth/guards/auth.guard.ts
+++ src/app/modules/auth/guards/auth.guard.ts
@@ -1,43 +1,24 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-  UrlTree,
-} from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service';
 import { map, take } from 'rxjs/operators';
-import { toast } from 'ngx-sonner';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
-    console.log("We're here");
-    return this.authService.currentUser$.pipe(
+  canActivate(): Observable<boolean> {
+    return this.afAuth.authState.pipe(
       take(1),
       map((user) => {
         if (user) {
-          const expectedRoles = next.data['roles'] as string[];
-          if (!expectedRoles || expectedRoles.includes(user.userType)) {
-            return true;
-          } else {
-            console.log(
-              `Unauthorized access for role: ${user.userType}, expected: ${expectedRoles}`
-            );
-            toast.error('Unauthorised');
-            return this.router.createUrlTree(['/unauthorized']);
-          }
+          return true;
         } else {
-          return this.router.createUrlTree(['/auth/login']);
+          this.router.navigate(['/auth/login']);
+          return false;
         }
       })
     );
